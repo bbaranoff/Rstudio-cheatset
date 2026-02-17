e1dcb36835753ad328ad2f46d60984ebccb64d0b
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2026-02-17 20:48:35 +0100

    Delete install-rstudio.sh

--- install-rstudio.sh
+++
@@ -1,9 +0,0 @@
-export DEBIAN_FRONTEND=noninteractive
-apt update && apt install -y software-properties-common texlive-xetex
-add-apt-repository -y ppa:c2d4u.team/c2d4u4.0+
-echo "deb https://cloud.r-project.org/bin/linux/ubuntu noble-cran40/" > /etc/apt/sources.list.d/cran.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-apt update && apt install -y r-cran-sass
-wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.12.1-563-amd64.deb
-sudo dpkg -i rstudio-2024.12.1-563-amd64.deb
-sudo apt -f install
